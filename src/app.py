f6fc2901ce408673930a520e203e38875ddb546f
ericsonmatheus <ericson_matheus.250@hotmail.com>
2025-11-30 14:34:12 -0300

    ajusta temporizado

--- src/app.py
+++ src/app.py
@@ -51,7 +51,7 @@ for i, row in enumerate(customers_page.iter_rows(min_row=2), start=0):
             f"https://web.whatsapp.com/send?phone={phone_number}&text={quote(message_to_send)}"
         )
         webbrowser.open(link_mensagem_whatsapp)
-        sleep(60)
+        sleep(180)
         trieds = 2
         error = None
         seta = None
